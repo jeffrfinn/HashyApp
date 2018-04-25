c1046233cd2b49c4f3f7228c439285a494065c43
Jeff <jeff.r.finn@gmail.com>
2018-04-25 22:00:15 +0100

    Add DoSomething component

--- HashyClient/src/app/app.module.ts
+++ HashyClient/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DoSomethingComponent } from './do-something/do-something.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoSomethingComponent
   ],
   imports: [
     BrowserModule
---
+++ HashyClient/src/app/do-something/do-something.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-do-something',
+  templateUrl: './do-something.component.html',
+  styleUrls: ['./do-something.component.css']
+})
+export class DoSomethingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
